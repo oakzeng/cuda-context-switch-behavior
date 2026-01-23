974fc8caf960fcfcf29ec13f50699c47d57a4244
Oak Zeng <oak.zeng@amd.com>
2026-01-23 10:23:31 -0500

    add a duration parameter to glmark2 mpi wrapper.c

--- glmark2_mpi_wrapper.c
+++ glmark2_mpi_wrapper.c
@@ -3,13 +3,19 @@
 #include <mpi.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 int main(int argc, char** argv) {
+    int duration = 1;
+    if (argc > 1) duration = atoi(argv[1]);
     MPI_Init(&argc, &argv);          // satisfy the runtime
     printf("glmark2 PID %d\n", getpid());
+    printf("glmark2 duration %d\n", duration);
+    char bench_arg[128];
+    snprintf(bench_arg, sizeof(bench_arg), "build:use-vbo=true:duration=%d", duration);
     // hand off to glmark2 (replace path if needed)
     MPI_Barrier(MPI_COMM_WORLD);
-    execlp("glmark2", "glmark2", "-b", "build:use-vbo=true:duration=1", (char*)NULL);
-    //execlp("glmark2", "glmark2", "-b", "ideas:use-vbo=true:duration=2", (char*)NULL);
+    execlp("glmark2", "glmark2", "-b", bench_arg, (char*)NULL);
     MPI_Finalize();
     return 0;
 }
